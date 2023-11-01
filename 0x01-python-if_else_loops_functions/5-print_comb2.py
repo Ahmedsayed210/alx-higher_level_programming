3a3a16aa8e285814f72966261fffaeedbbd0c1a7
Ahmedsayed210 <romaahmed791@gmail.com>
2023-11-01 12:10:08 -0700

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-num = 0
-while num <= 99:
-        print(f"{num:02}"",", end=" ")
-        num += 1
+for i in range(0, 99):
+        print("{:02d}".format(i), end=', ')
+        print("{:02d}".format(i + 1))
