33daab957ec33f7798eb7ef0bdbd14a5f95ccb78
Ahmedsayed210 <romaahmed791@gmail.com>
2023-11-02 16:40:52 -0700

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    import sys
+    sum1 = len(sys.argv) -1
+    if sum1 < 1:
+        print("0 arguments.")
+    elif sum1 == 1:
+        print("1 arguments.")
+    else:
+        print("{} arguments:".format(sum1))
+    for i in range(sum1):
+            print("{} : {}".format(i + 1,sys.argv[i + 1]))
+
+
