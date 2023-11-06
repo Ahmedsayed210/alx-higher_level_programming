7cf071a9131e295216bbf10a6c59c9b022eaec9e
Ahmedsayed210 <romaahmed791@gmail.com>
2023-11-05 18:32:00 -0800

    task7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-  if tuple_a[0] == 0 or tuple_b[0] == 0:
+  if len(tuple_a) == 0 or len(tuple_b) == 0:
     return (0, 0)
   else:
     first = tuple_a[0] + tuple_b[0]
