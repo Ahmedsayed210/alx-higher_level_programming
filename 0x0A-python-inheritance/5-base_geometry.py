b90290ce800dbe4bd6fc04245019e140f179d1bc
Ahmedsayed210 <romaahmed791@gmail.com>
2023-12-04 13:14:25 -0800

    task5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module for BaseGeometry class.'''
+
+
+class BaseGeometry:
+    '''
+        A base class for geometry-related operations.
+        This class is currently empty and serves as a placeholder.
+    '''
+    pass
