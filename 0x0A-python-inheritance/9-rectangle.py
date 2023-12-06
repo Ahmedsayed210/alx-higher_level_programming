d9dc28865a99fda3f0231008baab54f74c0a81a2
Ahmedsayed210 <romaahmed791@gmail.com>
2023-12-06 14:59:02 -0800

    task9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''MOdule for Rectangle method.'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculate and return the area of the rectangle.
+
+        Returns:
+        - float or int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.width}/{self.height}"
+
+    def __repr__(self):
+        """
+        Return a detailed representation of the rectangle.
+
+        Returns:
+        - str: A string representation containing the width and
+        height of the rectangle.
+        """
+        return f"Rectangle(width={self.width}, height={self.height})"
