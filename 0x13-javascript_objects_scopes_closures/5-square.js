1a8f1779c2cc696a47480e5921807829615f35b9
Ahmedsayed210 <romaahmed791@gmail.com>
2024-02-13 21:40:45 +0200

    three tasks

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Square;
