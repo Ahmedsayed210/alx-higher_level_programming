3c203686b84b8d46e8ccb73a2ef4cd516e7188dc
Ahmedsayed210 <romaahmed791@gmail.com>
2024-02-13 17:44:00 +0200

    Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const [, , numArg] = process.argv;
+parseInt(numArg);
+const C = 'C is fun';
+if (!isNaN(numArg)){
+  for (let i = 0; i < numArg;i++){
+    console.log(C);
+  }
+}
+else{
+  console.log('Missing number of occurrences');
+}
