6ea080ddd50e23d7ace6a64b07d8258233daeae1
AhmedSayed210 <romaahmed791@gmail.com>
2024-02-24 03:31:40 +0200

    2024-02-24 03:31:40 ص

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host = "localhost",
+        user = sys.argv[1],
+        passwd = sys.argv,
+        db = sys.argv[3],
+        port = 3306
+    )
+    cur = db.cursors()
+    cur.execute("SELECT * FROM states WHRER name  LIKE BINARY '%N' ORDER BY states.id")
+    rows = cur.fetcall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
