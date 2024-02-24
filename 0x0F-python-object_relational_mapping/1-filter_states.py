22164f66d6331baca686908583433b45514783ee
AhmedSayed210 <romaahmed791@gmail.com>
2024-02-24 03:34:36 +0200

    2024-02-24 03:34:36 ص

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host = "localhost",
         user = sys.argv[1],
-        passwd = sys.argv,
+        passwd = sys.argv[2],
         db = sys.argv[3],
         port = 3306
     )
